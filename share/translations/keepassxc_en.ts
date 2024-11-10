9b8163c3a46cb1b8c3fb8ded1a93aedd12de049c
Jonathan White <support@dmapps.us>
2024-11-10 09:10:27 -0500

    Use better xref syntax for documentation

--- share/translations/keepassxc_en.ts
+++ share/translations/keepassxc_en.ts
@@ -682,19 +682,6 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Invalid conversion type: %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Invalid conversion syntax: %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Invalid regular expression syntax %1
-%2</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Invalid placeholder: %1</source>
         <translation type="unfinished"></translation>
     </message>
@@ -3812,6 +3799,19 @@ This may cause the affected plugins to malfunction.</source>
     </message>
     <message>
         <source>Passkey</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid conversion type: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid conversion syntax: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid regular expression syntax %1
+%2</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
